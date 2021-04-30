d18fe83a4ef2e802d221d4741276b334dbb3b5e7
Jhon Mosk <jhon-mosk@yandex.ru>
2021-04-30 20:43:57 +0300

    comment

--- Lesson8/index.js
+++ Lesson8/index.js
@@ -4,12 +4,12 @@
 if (!("a" in window)) {
     var a = 1;
 }
-alert(a); //undefined
+alert(a); //undefined, так как условие if не сработало
 
 var b = function a(x) {
     x && a(--x);
 };
-alert(a); //undefined
+alert(a); //undefined, так как функция задана через переменную b
 
 function a(x) {
     return x * 2;
@@ -19,15 +19,15 @@ alert(a);
 /*function a(x) {
     return x * 2;
 }
-*/
+*/ //так как в переменной а ссылка на функцию
 
 function b(x, y, a) {
     arguments[2] = 10;
     alert(a);
 }
-b(1, 2, 3); //10
+b(1, 2, 3); //10 так как третьему аргументу было присвоено значение 10
 
 function a() {
     alert(this);
 }
-a.call(null); // window
+a.call(null); // window так как с помощью call this было спрошено до null, а самый главный элемент это window
